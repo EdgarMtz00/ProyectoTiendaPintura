312c7f469270fc159ab940b74d80d14aea282f12
Edgar_Mtz00 <edgar.agustin.martinez@gmail.com>
2018-05-25 20:21:41 -0500

    Add RecycleView

--- app/src/main/java/com/example/edgarmartinez/proyectotiendapintura/MainActivity.java
+++ app/src/main/java/com/example/edgarmartinez/proyectotiendapintura/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.edgarmartinez.proyectotiendapintura;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AbsListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        articuloAdapter adapter = new articuloAdapter();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter.context=this;
+        //adapter.data=origeDeDatos;
+        //recyclerView.setAdapter(adapter);
+
     }
-    public void articuloClick(View v){
-        Intent articuloIntent = new Intent(getApplicationContext(), Articulo.class);
-        startActivity(articuloIntent);
-    }
+
 }
